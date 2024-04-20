052d1f874f737d0cc803417ee027bbe8054912b4
VishShindhe <vishwa.shindhe@gmail.com>
2024-04-21 00:47:59 +0530

    Adding support for multiple browsers using Selenium Manager    - Firefox    - Chrome

--- completeseleniumproj/src/main/java/com/vish/driver/Driver.java
+++ completeseleniumproj/src/main/java/com/vish/driver/Driver.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import com.vish.enums.ConfigProperties;
 import com.vish.utils.PropertyUtils;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import com.vish.constants.FrameworkConstants;
 
@@ -12,17 +13,11 @@ public final class Driver {
      public static void initDriver(String browser) throws Exception {
         if(Objects.isNull(DriverManager.getDriver())){
             if(browser.equalsIgnoreCase("firefox")) {
-                System.setProperty("webdriver.gecko.driver", FrameworkConstants.getFirefoxDriverPath());
                 DriverManager.setDriver(new FirefoxDriver());
-
             }
-
-            /* Add for other browsers
             else if(browser.equalsIgnoreCase("chrome")) {
-                System.setProperty("webdriver.chrome.driver", FrameworkConstants.getChromeDriverPath());
                 DriverManager.setDriver(new ChromeDriver());
             }
-             */
 
             DriverManager.getDriver().get(PropertyUtils.get(ConfigProperties.URL).trim());
         }
