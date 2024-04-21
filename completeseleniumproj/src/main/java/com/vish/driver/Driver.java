e75ab982ece52a49599aee006287cb73c3f2db72
VishShindhe <vishwa.shindhe@gmail.com>
2024-04-21 16:46:21 +0530

    Dockerising project

--- completeseleniumproj/src/main/java/com/vish/driver/Driver.java
+++ completeseleniumproj/src/main/java/com/vish/driver/Driver.java
@@ -1,5 +1,6 @@
 package com.vish.driver;
 
+import java.net.URL;
 import java.util.Objects;
 
 import com.vish.enums.ConfigProperties;
@@ -7,15 +8,29 @@ import com.vish.utils.PropertyUtils;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import com.vish.constants.FrameworkConstants;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public final class Driver {
      private Driver() {}
      public static void initDriver(String browser) throws Exception {
+         String runMode = PropertyUtils.get(ConfigProperties.RUNMODE);
         if(Objects.isNull(DriverManager.getDriver())){
             if(browser.equalsIgnoreCase("firefox")) {
+                if(runMode.equalsIgnoreCase("remote")) {
+                    DesiredCapabilities cap = new DesiredCapabilities();
+                    cap.setBrowserName("firefox");
+                    DriverManager.setDriver(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), cap));
+                }
                 DriverManager.setDriver(new FirefoxDriver());
             }
             else if(browser.equalsIgnoreCase("chrome")) {
+                if(runMode.equalsIgnoreCase("remote")) {
+                    DesiredCapabilities cap = new DesiredCapabilities();
+                    cap.setBrowserName("chrome");
+                    DriverManager.setDriver(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap));
+                }
                 DriverManager.setDriver(new ChromeDriver());
             }
 
